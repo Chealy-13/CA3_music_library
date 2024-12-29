7ba679aa8b0279a3f0313d9e702aea77a9d5819e
sophie <d00259237@student.dkit.ie>
2024-12-29 18:20:33 +0000

    Created index and error handling controllers

---
+++ src/main/java/controllers/IndexController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+    @GetMapping("/")
+    public String home() { return "index";}
+
+    @GetMapping("/user_index")
+    public String userIndex() { return "user_index";}
+
+    @GetMapping("/Song_index")
+    public String songIndex(){ return "Song_index"; }
+
+    @GetMapping("/PlayList_index")
+    public String playlistIndex(){ return "PlayList_index"; }
+
+    @GetMapping("/artist_index")
+    public String artistIndex(){ return "artist_index"; }
+
+    @GetMapping("/album_index")
+    public String albumIndex(){ return "album_index"; }
+
+}
