38270061f36b1e6cd5f12fa93d7d7a497f961b3c
Damian0222 <d00229247@student.dkit.ie>
2025-01-04 19:38:04 +0000

    Added back the methods from the previous project(updated) - userdaoImpl class

--- src/main/java/Persistence/UserDaoImpl.java
+++ src/main/java/Persistence/UserDaoImpl.java
@@ -147,3 +147,186 @@ public class UserDaoImpl extends MySQLDao implements UserDao {
         return false;
     }
 }
+
+//@Override
+//public boolean RegisterU(String username, String password, String email) {
+    //This ine is used instert row to "users" table with values: useranme, password, email.
+  //  public boolean registerUser(String username, String password, String email) {
+    //    String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+      //  try (PreparedStatement state = getConnection().prepareStatement(sql)) {
+            // Set the first '?' placeholder in the SQL query to the user's username
+        //    state.setString(1, username);
+            // Set the second '?' placeholder in the SQL query to the user's password
+          //  state.setString(2, password);
+            // Set the third '?' placeholder in the SQL query to the user's email
+            //state.setString(3, email);
+            //return state.executeUpdate() > 0;
+            //try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+              //  statement.setString(1, username);
+                //statement.setString(2, password);
+                //statement.setString(3, email);
+
+                //return statement.executeUpdate() > 0;
+            //} catch (SQLException e) {
+              //  System.out.println("SQL Exception occurred when attempting to prepare SQL for execution.");
+                //System.out.println("Error: " + e.getMessage());
+                //e.printStackTrace();
+                //System.err.println("Error during user registration: " + e.getMessage());
+                //return false;
+          //  }
+            // Return false if fails or an exception occurs
+            //return false;
+        //}
+/**
+ * Collects a user from the database by username.
+ * The method gets the SQL query to select all fields from the 'users' table,
+ * where it matches the username. If a match is found,
+ * a User object is created and returned with the user's information. If no
+ * users are found, or if an exception occurs the method will return null.
+ *
+ * @param username the username of the user to be retrieved from the database.
+ * @return a User object if found, or if no matching user is found.
+ * @throws SQLException if there is an error executing the SQL statement or retrieving the results.
+ */
+//@Override
+//public User loginUser(String username) {
+//String sql = "SELECT * FROM users WHERE username = ?";
+//try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+//statement.setString(1, username);
+//try (ResultSet resultSet = statement.executeQuery()) {
+//if (resultSet.next()) {
+//return new User(
+//      resultSet.getInt("userId"),
+//        resultSet.getString("username"),
+//          resultSet.getString("password"),
+//            resultSet.getString("email")
+//      );
+//    }
+//  }
+//} catch (SQLException e) {
+//  System.err.println("Error during user login: " + e.getMessage());
+//}
+//  return null;
+//}
+
+/**
+ * Checks if a username is available.
+ *
+ * @param username the username to check.
+ * @return true if the username is available, false otherwise.
+ */
+// public boolean isUsernameAvailable(String username) {
+//String sql = "SELECT COUNT(*) FROM users WHERE username = ?";
+//try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+//statement.setString(1, username);
+//try (ResultSet resultSet = statement.executeQuery()) {
+//  if (resultSet.next()) {
+//        return resultSet.getInt(1) == 0;
+//      }
+//  }
+//} catch (SQLException e) {
+//  System.err.println("Checking username availability: " + e.getMessage());
+//}
+//  return false;
+//}
+
+/**
+ * Checks if an email is available.
+ *
+ * @param email the email to check.
+ * @return true if the email is available, false otherwise.
+ */
+//public boolean isEmailAvailable(String email) {
+//String sql = "SELECT COUNT(*) FROM users WHERE email = ?";
+//try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+//statement.setString(1, email);
+//try (ResultSet resultSet = statement.executeQuery()) {
+//    if (resultSet.next()) {
+//          return resultSet.getInt(1) == 0;
+//        }
+//      }
+//    } catch (SQLException e) {
+//      System.err.println("Checking email availability: " + e.getMessage());
+// }
+//return false;
+//}
+
+
+/**
+ * Deletes user based on username from database
+ * This method executes an SQL DELETE statement to remove a user from the
+ * 'users' table.
+ * If the deletion is successful, it returns true; otherwise, it returns false.
+ *
+ * @param username of user will be deleted
+ * @return true if the user was successfully deleted, false otherwise or if an error occurred
+ */
+
+//@Override
+//public boolean deleteByUsername(String username) {
+//Sql delete query is used to remove a user with specified username
+//  String sql = "DELETE FROM users WHERE username = ?";
+//This line is used to stop sql injection and handle parameters safely
+//      try (PreparedStatement state = getConnection().prepareStatement(sql)) {
+//Username provided as a method parameter will replace the '?'
+//       state.setString(1, username);
+//This line runs the delete operation, it returns the number of rows affected
+//     int rowChoice = state.executeUpdate();
+//   return rowChoice > 0;
+//} catch (SQLException E) {
+//  System.out.println("SQL Exception occurred when attempting to prepare SQL for execution.");
+//System.out.println("Error: " + E.getMessage());
+//E.printStackTrace();
+//}
+//return false;
+//}
+
+/**
+ * Extracts a user object from the ResultSet.
+ *
+ * @param resultSet the ResultSet containing user data.
+ * @return a User object populated with the data from the ResultSet.
+ */
+//  private User extractUserFromResultSet(ResultSet resultSet) throws SQLException {
+//    return new User(
+//          resultSet.getInt("userId"),
+//        resultSet.getString("username"),
+//      resultSet.getString("password"),
+//    resultSet.getString("email"));
+//    }
+
+//}
+/**
+ * Collects all users from the 'users' table in the database.
+ * The resulting list of users,
+ * If no users are found, an empty list is returned.
+ *
+ * @return a List of {user} objects representing all users in the database.
+ * Returns nothing if list is empty.
+ */
+//   public List<user> getAllUsers() {
+// Creates a new Arraylist ro store objects
+//     List<user> users = new ArrayList<>();
+//   String sql = "SELECT * FROM users";
+// try (PreparedStatement statement = connection.prepareStatement(sql)) {
+//   try (ResultSet rs = statement.executeQuery()) {
+//     while (rs.next()) {
+//       user user = new user(
+//             rs.getInt("userId"),
+//           rs.getString("username"), rs.getString("password"),
+//         rs.getString("email")
+// );
+//users.add(user);
+//}
+//} catch (SQLException e) {
+//  System.out.println("SQL Exception occurred when executing SQL or processing results.");
+//System.out.println("Error: " + e.getMessage());
+//e.printStackTrace();
+//}
+//} catch (SQLException e) {
+//   System.out.println("SQL Exception occurred when attempting to prepare SQL for execution");
+//     System.out.println("Error: " + e.getMessage());
+//       e.printStackTrace();
+//     }
+//   return users;
+//}
