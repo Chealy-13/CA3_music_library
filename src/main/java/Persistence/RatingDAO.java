4bf21387bf5d2871a8568f2ebf39cf33de7de7e0
Chealy-13 <chris.healy33@gmail.com>
2025-01-05 19:26:23 +0000

    Refactored previous ratingdao and ratingdaoimpl and renamed some fields.

--- src/main/java/Persistence/RatingDAO.java
+++ src/main/java/Persistence/RatingDAO.java
@@ -1,11 +1,13 @@
 package Persistence;
 
+import business.Rating;
+
 import java.sql.SQLException;
 import java.util.List;
 
 public interface RatingDAO {
 
     boolean rateSong(int songId_, int userId_, int rating_)throws SQLException;
-    List<RatingDAO> getRatingsByUser(int userId) throws SQLException;
+    List<Rating> getRatingsByUser(int userId) throws SQLException;
 
 }
--- src/main/java/Persistence/RatingDAOImpl.java
+++ src/main/java/Persistence/RatingDAOImpl.java
@@ -19,19 +19,19 @@ public class RatingDAOImpl implements RatingDAO {
     /**
      * Rates a song and inserts into the Ratings table.
      * This method takes the user's rating for a song, ratings from range from 1 to 5.
-     * @param songId_ The ID of the song being rated.
-     * @param userId_ The ID of the user giving the rating.
-     * @param rating_ The rating value.
+     * @param songId The ID of the song being rated.
+     * @param userId The ID of the user giving the rating.
+     * @param rating The rating value.
      * @return true if the rating was successfully added to the database, false otherwise.
      * @throws SQLException if a database access error occurs or the SQL problem.
      */
     @Override
-    public boolean rateSong(int songId_, int userId_, int rating_) throws SQLException {
-        String sql = "INSERT INTO Ratings (songId, userId, rating) VALUES (?, ?, ?)";
+    public boolean rateSong(int userId, int songId, int rating) throws SQLException {
+        String sql = "INSERT INTO Ratings (userId, songId, rating) VALUES (?, ?, ?)";
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setInt(1, songId_);
-            ps.setInt(2, userId_);
-            ps.setInt(3, rating_);
+            ps.setInt(1, userId);
+            ps.setInt(2, songId);
+            ps.setInt(3, rating);
             return ps.executeUpdate() > 0;
 
         } catch (SQLException e) {
@@ -46,24 +46,26 @@ public class RatingDAOImpl implements RatingDAO {
      * This method executes a SQL query to collect all ratings associated with the
      * user. It takes a list of Rating
      * objects, each representing a rating associated with a song.
-     * @param userId_ the ID of the user whose ratings are to be collected
+     *
+     * @param userId the ID of the user whose ratings are to be collected
      * @return a list of Rating objects containing the song IDs, user IDs, and ratings
      * for the specified user. Returns an empty list if non are found.
      * @throws SQLException if a database access error occurs or the SQL statement is wrong
      */
-    public List<RatingDAO> getRatingsByUser(int userId_) throws SQLException {
-        String sql = "SELECT songId, userId, rating FROM Rating WHERE userId = ?";
+    public List<Rating> getRatingsByUser(int userId) throws SQLException {
+        String sql = "SELECT * FROM ratings WHERE userId = ?";
 
         List<Rating> ratings = new ArrayList<>();
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setInt(1, userId_);
+            ps.setInt(1, userId);
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
-                    ratings.add(new Rating(
-                            rs.getInt("songID"),
-                            rs.getInt("userID"),
-                            rs.getInt("rating")
-                    ));
+                    int ratingId = rs.getInt("ratingId");
+                    int songId = rs.getInt("songId");
+                    int userID = rs.getInt("userId");
+                    int rating = rs.getInt("rating");
+
+                    ratings.add(new Rating(ratingId, songId, userID, rating));
                 }
             }
         } catch (SQLException e) {
@@ -71,6 +73,6 @@ public class RatingDAOImpl implements RatingDAO {
             System.out.println("Error: " + e.getMessage());
             e.printStackTrace();
         }
-        return null;
+        return ratings;
     }
 }
