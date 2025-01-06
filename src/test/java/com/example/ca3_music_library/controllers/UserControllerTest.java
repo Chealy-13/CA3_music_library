b6c2f80dbad849e9bf26994cace521d10cb95002
Damian0222 <d00229247@student.dkit.ie>
2025-01-06 20:04:14 +0000

    Added some tests

--- src/test/java/com/example/ca3_music_library/controllers/UserControllerTest.java
+++ src/test/java/com/example/ca3_music_library/controllers/UserControllerTest.java
@@ -2,7 +2,6 @@ package com.example.ca3_music_library.controllers;
 
 
 import com.example.ca3_music_library.Persistence.UserDao;
-import com.example.ca3_music_library.business.User;
 import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,15 +17,33 @@ class UserControllerTest {
     private Model model;
     private HttpSession session;
 
+
     @Test
     void testRegister_PasswordsDoNotMatch() {
         String username = "testUser";
         String password = "Password123";
-        String confirmPassword = "Password456";
+        String confirmPassword = "Password6";
 
         String result = userController.register(username, password, confirmPassword, null, null, "test@example.com", model, session);
 
         Mockito.verify(model).addAttribute("errorMessage", "Passwords must match!");
         assertEquals("registration", result);
+    }
+
+    @Test
+    void testRegister_PasswordWithoutUppercaseOrNumber() {
+        HttpSession session = mock(HttpSession.class);
+        Model model = mock(Model.class);
+
+        String username = "Damian";
+        String password = "password";
+        String confirmPass = "password";
+        String first = "Damian";
+        String last = "Magiera";
+        String email = "test@example.com";
+
+        String result = userController.register(username, password, confirmPass, first, last, email, model, session);
+        assertEquals("registration", result, "Should go back to registration page for invalid password.");
+        verify(model).addAttribute("errorMessage", "Password must contain at least one uppercase letter and one number!");
     }
 }
