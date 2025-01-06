fedc0426606e5370e306e635db6c2b25a075e1d3
sophie <d00259237@student.dkit.ie>
2025-01-06 15:24:16 +0000

    Added Javadocs to Song and Artist Controllers

--- src/main/java/com/example/ca3_music_library/controllers/ArtistController.java
+++ src/main/java/com/example/ca3_music_library/controllers/ArtistController.java
@@ -15,10 +15,22 @@ public class ArtistController {
 
     private final ArtistDao artistDao;
 
+    /**
+     * Constructs ArtistController with the specified ArtistDao.
+     *
+     * @param artistDao  instance used for artist-related database operations
+     */
     public ArtistController(ArtistDao artistDao){
         this.artistDao = artistDao;
     }
 
+    /**
+     * Handles requests to the "/artists" endpoint.
+     * Retrieves all artists from the database and adds them to the model for rendering in the "artists" view.
+     *
+     * @param model used to pass data to the view
+     * @return the name of the Thymeleaf template ("artists") to render
+     */
     @GetMapping("/artists")
     public String viewArtists(Model model){
      List<Artist> artists = artistDao.getAllArtists();
@@ -26,6 +38,15 @@ public class ArtistController {
      return "artists";
     }
 
+    /**
+     * Handles requests to the "/viewArtist/{id}" endpoint.
+     * Retrieves the details of a specific artist and their songs from the database based on the artist ID.
+     * Adds the artist details and their songs to the model for rendering in the "artistDetails" view.
+     *
+     * @param artistId the ID of the artist to retrieve
+     * @param model used to pass data to the view
+     * @return the name of the Thymeleaf template ("artistDetails")
+     */
     @GetMapping("/viewArtist/{id}")
     public String viewArtist(@PathVariable("id") int artistId, Model model) {
         Artist artist = artistDao.getArtistByArtistId(artistId);
